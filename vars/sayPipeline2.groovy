84551a2b6cd78445ab6cb7b13b5321c2652fc276
sabarivasan1 <sabari.vasan1@gmail.com>
2021-09-07 19:47:30 +0200

    Update sayPipeline2.groovy

--- vars/sayPipeline2.groovy
+++ vars/sayPipeline2.groovy
@@ -5,7 +5,7 @@ def call(Map params) {
 
 			def sanityMap = input id: 'SanityId', message: 'Should we continue?', ok: 'Yes, we should.', submitter: 'Alice, Noob', parameters: [string(defaultValue: '',description: '', name: 'Username'), string(defaultValue: '', description: '', name: 'Password'), booleanParam(description: '', name: 'Remain'),choice(choices:'ONE\nTWO', description: '', name: 'Choosen')]
 build(
-									  job: 'ProjectF',
+									  job: 'ProjectD',
 									  parameters: [
 										[
 										  $class: 'StringParameterValue',
