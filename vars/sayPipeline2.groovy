d3e0485f571ba70c9ef3dbd8bb1908b09f7e06ca
sabarivasan1 <sabari.vasan1@gmail.com>
2021-09-07 19:25:46 +0200

    Update sayPipeline2.groovy

--- vars/sayPipeline2.groovy
+++ vars/sayPipeline2.groovy
@@ -3,25 +3,6 @@ import com.cleverbuilder.GlobalVars
 import com.cleverbuilder.SampleClass
 def call(Map params) {
 
-pipeline {
-    agent any
-    stages {
-        stage('Demo') {
-            steps {
-                echo 'Hello, world 2'
-                sayHello 'Dave 2'
+			def sanityMap = input id: 'SanityId', message: 'Should we continue?', ok: 'Yes, we should.', submitter: 'Alice, Noob', parameters: [string(defaultValue: '',description: '', name: 'Username'), string(defaultValue: '', description: '', name: 'Password'), booleanParam(description: '', name: 'Remain'),choice(choices:'ONE\nTWO', description: '', name: 'Choosen')]
 
-                echo 'The value of foo is : ' + GlobalVars.foo
-
-                script {
-                    def person = new SampleClass()
-                    person.age = 43
-                    person.increaseAge(10)
-                    echo 'Incremented age, is now : ' + person.age
-                    sayAge person.age
-                }
-            }
-        }
-    }
-}
 }
