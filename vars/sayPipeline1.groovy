21eb994efe4fcc123a4eccd2759a8f1b80f52bbb
sabarivasan1 <sabari.vasan1@gmail.com>
2021-09-07 19:25:17 +0200

    Update sayPipeline1.groovy

--- vars/sayPipeline1.groovy
+++ vars/sayPipeline1.groovy
@@ -23,25 +23,4 @@ pipeline {
         }
     }
 }
-    pipeline {
-    agent any
-    stages {
-        stage('Demo') {
-            steps {
-                echo 'Hello, world 1'
-                sayHello 'Dave 1'
-
-                echo 'The value of foo is : ' + GlobalVars.foo
-
-                script {
-                    def person = new SampleClass()
-                    person.age = 21
-                    person.increaseAge(10)
-                    echo 'Incremented age, is now : ' + person.age
-                    sayAge person.age
-                }
-            }
-        }
-    }
-}
 }
