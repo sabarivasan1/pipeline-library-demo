b59c2cf0d044a002c79f97e227af27b22a79ab34
sabarivasan1 <sabari.vasan1@gmail.com>
2021-09-08 00:50:44 +0200

    Update sayPipeline1.groovy

--- vars/sayPipeline1.groovy
+++ vars/sayPipeline1.groovy
@@ -8,13 +8,13 @@ pipeline {
         stage('Demo') {
             steps {
                 echo 'Hello, world 1'
-                sayHello 'Dave 1'
+                sayHello "${name}"
 
                 echo 'The value of foo is : ' + GlobalVars.foo
 
                 script {
                     def person = new SampleClass()
-                    person.age = 21
+                    person.age = "${age}"
                     person.increaseAge(10)
                     echo 'Incremented age, is now : ' + person.age
                     sayAge person.age
